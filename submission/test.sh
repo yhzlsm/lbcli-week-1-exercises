ab5d7143e7ab8194e1fed688ba3c94a3d72e718a
yhzlsm <yhzlsm@gmail.com>
2025-03-20 09:20:01 -0300

    fix: collected calculation

--- submission/test.sh
+++ submission/test.sh
@@ -101,7 +101,7 @@ NEW_BALANCE=$(bitcoin-cli -regtest -rpcwallet=treasurewallet getbalance)
 check_cmd "New balance check"
 echo "Your treasure balance: $NEW_BALANCE BTC"
 
-COLLECTED=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getbalance) - $NEW_BALANCE
+COLLECTED=$(($(bitcoin-cli -regtest -rpcwallet=btrustwallet getbalance) - $NEW_BALANCE))
 check_cmd "Balance calculation"
 echo "You've collected $COLLECTED BTC in treasures!"
 
